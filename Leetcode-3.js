3a1b3dc891a3de20abfc6a04c2dc21533de86078
burcuoksuz7 <67266830+burcuoksuz7@users.noreply.github.com>
2020-06-28 19:34:04 +0300

    Create Leetcode-3.js

---
+++ Leetcode-3.js
@@ -0,0 +1,23 @@
+var lengthOfLongestSubstring = function(s) {
+    
+};
+
+var lengthOfLongestSubstring = function(s) {
+    var length = s.length;
+    if (length <= 1) {
+        return length;
+    }
+    var p = 0;
+    var q = 1;
+    var result = 1;
+    while (q < length) {
+        var sub = s.slice(p, q);
+        var i = sub.indexOf(s[q]);
+        if (i !== -1) {
+            p = p + i + 1;
+        }
+        q++;
+        result = q - p > result ? q - p : result;
+    }
+    return result;
+};
